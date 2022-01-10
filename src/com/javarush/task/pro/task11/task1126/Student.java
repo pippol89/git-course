0a470436bcd41ca91521cd032cf19ca5c20d7463
Andrey D <seedorf10@rambler.ru>
2022-01-10 20:55:50 +0300

    first commit

---
+++ src/com/javarush/task/pro/task11/task1126/Student.java
@@ -0,0 +1,29 @@
+package com.javarush.task.pro.task11.task1126;
+
+/*
+Студент
+*/
+
+public class Student {
+    public String name;
+
+    public void f1(int a)
+    {
+        //
+    }
+
+    public void f1(float a)
+    {
+        //
+    }
+
+    /*public int f1(float a)
+    {
+
+        return 0;
+    }*/
+
+    public Student(String name) {
+        this.name = name;
+    }
+}
